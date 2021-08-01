b0fd89d825735bebcac9d00362e1e11eda6ba9aa
ciyer87 <chitra.iyer00@gmail.com>
2021-07-31 17:14:42 -0700

    fixed css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,19 +16,19 @@ function Footer() {
             <div>
                 <a href="https://github.com/ciyer87" target="_blank" rel="noreferrer">
                     {/* <FontAwesomeIcon icon={["fab", "git-square"]} size="3x" color="#607d8b" /> */}
-                    <i class="fab fa-git-square" size="3x" color="#607d8b"></i>
+                    <i className="fab fa-git-square" size="3x" color="#607d8b"></i>
                 </a>
             </div>
             <div>
                 <a href="https://www.linkedin.com/in/chitra-padmanabhan-2404812/" target="_blank" rel="noreferrer">
                     {/* <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" color="#607d8b" /> */}
-                    <i class="fab fa-linkedin"></i>
+                    <i className="fab fa-linkedin"></i>
                 </a>
             </div>
             <div>
                 <a href="https://stackoverflow.com/users/16558724/chitra" target="_blank" rel="noreferrer">
                     {/* <FontAwesomeIcon icon={["fab", "stack-overflow"]} size="3x" color="#607d8b" /> */}
-                    <i class="fab fa-stack-overflow"></i>
+                    <i className="fab fa-stack-overflow"></i>
                 </a>
             </div>
         </footer>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,76 +8,88 @@ function Portfolio() {
             alt: 'goodvice blog',
             title: 'goodvice blog',
             description: 'A blog site to get input from audience',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(Express.js/Node.js/MySQL2/CSS)',
+            url: 'https://the-goodvice-blog.herokuapp.com/',
+            git: 'https://github.com/crossigarcia/the-goodvice-blog'
         },
         {
             image: 'password.png',
             alt: 'password generator',
             title: 'password generator',
             description: 'An app to generate random password',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(HTML/CSS/JS)',
+            url: 'https://ciyer87.github.io/password-generator/',
+            git: 'https://github.com/ciyer87/password-generator/'
         },
         {
             image: 'weather.png',
             alt: 'Weather Vane',
             title: 'weather dashboard',
             description: 'An app to view the current and 5 day weather forecast based on the city entered by user',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(OpenWeather API/HTML/CSS)',
+            url: 'https://ciyer87.github.io/Weather-Dashboard/',
+            git: 'https://github.com/ciyer87/Weather-Dashboard/'
         },
         {
             image: 'todo_color_variation.png',
             alt: 'notetaker app',
             title: 'notetaker app',
             description: 'An app to keep track of to-do list',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(jQuery/Moment.js/Bootstrap)',
+            url: 'https://safe-crag-82447.herokuapp.com/',
+            git: 'https://github.com/ciyer87/note-taker'
         },
         {
             image: 'javascript-quiz.png',
             alt: 'quiz',
             title: 'javascript quiz',
             description: 'An online multiple choice quiz app',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(HTML/CSS/JavaScript/WebAPI)',
+            url: 'https://ciyer87.github.io/Javascript-Quiz/',
+            git: 'https://github.com/ciyer87/Javascript-Quiz'
         },
         {
             image: 'group-project-1.png',
             alt: 'recipe app',
             title: 'recipe app',
             description: 'An recipe app that allows users to search based on ingredients or category',
-            code: 'test',
-            url: '',
-            git: ''
+            code: '(HTML/CSS/JQuery/UIKit/3rd party API)',
+            url: 'https://nicopetelo.github.io/whats-for-dinner/',
+            git: 'https://github.com/nicopetelo/whats-for-dinner'
         }
     ])
     return (
-       <section>
-        <h1 className="title">Portfolio</h1>
-                {projects.map((project, i) =>
-                    <div className="flex-row" key={i}>
-                        <img src={require(`../../assets/images/${project.image}`).default} alt={project.alt} />
-                        <div className="content">
-                            <h3>{project.title}</h3>
-                            <p>{project.description}<br /><span>{project.code}</span></p>
-                            <a href={project.url} target="_blank" rel="noreferrer">View Site</a>
-                            <a href={project.git} target="_blank" rel="noreferrer">
-                                <FontAwesomeIcon icon={["fab", "github"]} size="1x" color="#ffffff" />
-                            </a>
+       <div className="container">
+            <h2 className="title">Portfolio</h2>
+                <div className="main">
+                    {projects.map((project, i) =>
+                        <div className="picture" key={i}>
+                            <img src={require(`../../assets/images/${project.image}`).default} alt={project.alt} />
+                                <div className="overlay">
+                                    <div className="overlaytext">
+                                        <h3>{project.title}</h3>
+                                        <p>{project.description}<br /><span>{project.code}</span></p>
+                                        <a href={project.url} target="_blank" rel="noreferrer">View Site</a>
+                                        <a href={project.git} target="_blank" rel="noreferrer">
+                                        {/* <FontAwesomeIcon icon={["fab", "github"]} size="1x" color="#ffffff" /> */}
+                                            <i className="fab fa-git-square"></i>
+                                        </a>
+                                    </div>    
+                                </div>    
 
-                        </div>
-                    </div>
-                )}
-            
-        </section>   
-    )
+                           
+                    
+                        </div>  
+                    )}       
+                </div>
+        </div>
+                
+        )
+    
 }
+            
+      
+    
+
 
 export default Portfolio;
